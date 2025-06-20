e52954dfc58abddcd02b195476f0d34f8d0145df
unknown <soporte@calima-4.ugca.local>
2025-06-20 09:16:55 -0500

    Este es mi primer commit

---
+++ datos.py
@@ -0,0 +1,11 @@
+n1 = int(input("Ingrese la primera variable: "))
+n2 = int(input("Ingrese la segunda variable: "))
+suma = n1 + n2
+resta = n1 - n2
+multiplicación = n1 * n2
+división = n1 / n2
+print("El resultadp de la suma es: ", suma)
+print("El resultado de la resta es: ", resta)
+print("El resultado de la multiplicación es: ", multiplicación)
+print("El resultado de la división es: ", división)
+
